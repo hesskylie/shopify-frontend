378cdbd8a5b8c44481565d01fdb47382a7b9c314
Kylie Hess <kyliemhess@gmail.com>
2021-01-17 06:55:04 -0600

    deployment tweaking

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/shopify-frontend', { logging: false });
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/shopify-frontend', {
+dialect: 'postgres',
+protocol: 'postgres',
+logging: false
+ });
 
 
 module.exports = db;
